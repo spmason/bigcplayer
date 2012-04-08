a5bf54c888752a502b0980afe1770b6fcde2fdc0
Steve Mason <steve@spmason.com>
2012-04-08 16:21:05 +0100

    Implemented fetching of prev/next pages

--- src/player.js
+++ src/player.js
@@ -5,10 +5,23 @@
         ["https://raw.github.com/soundcloud/Widget-JS-API/master/soundcloud.player.api.js", 'soundcloud'],
         ["https://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js", 'swfobject']
     ],
-        $container,
+        $container, $ul,
+        links = [],
+        startIndex = 2,
         depsLoaded = 0;
 
     function play(url){
+        var id = "bigcplayer_player",
+            player = document.getElementById(id) ? soundcloud.getPlayer(id) : undefined;
+
+        if(!player){
+            loadPlayer(url);
+        }else{
+            player.api_load(url);
+        }
+    }
+
+    function loadPlayer(url){
         var $player = $container.find('.player'),
             id = "bigcplayer_player",
             flashvars = {
@@ -24,33 +37,63 @@
                 id: id,
                 name: id
             };
+
         $player.html('<div id=' + id + ' />');
         swfobject.embedSWF("http://player.soundcloud.com/player.swf", id, "100%", "81", "9.0.0","expressInstall.swf", flashvars, params, attributes);
     }
 
-    function go(){
-        $container = $('<div class="bigcplayer"><a href class=toggle>Open Player</a><ul /><div class=player /></div>');
-        var $ul = $container.find('ul').css({padding:0,height:80,overflowY:'auto'}),
-            links = [];
-        $('a[href*="soundcloud.com"]').each(function(){
+    function loadLinks(doc, index){
+        doc.find('a[href*="soundcloud.com"]').each(function(){
             var $a = $(this),
-                href = $a.attr('href').toLowerCase();
+                href = $a.attr('href').toLowerCase(),
+                $el;
 
             if(links.indexOf(href) > -1 || href.indexOf('/download') > -1){
                 return;
             }
-            $ul.append($('<li />').html($('<a/>').attr('href', href).text($a.text())));
+            $el = $('<li />').html($('<a/>').attr('href', href).text($a.text()));
+            if(index > startIndex){
+                $ul.append($el);
+            }else{
+                $ul.prepend($el);
+            }
             links.push(href);
         });
+        $container.find('a')
+            .css({
+                color: '#FFF',
+                fontSize: 11
+            });
+
+        loadOtherPages(doc);
+    }
+
+    function loadOtherPages(doc){
+        doc.find('#navigation a[href*="page/"]').each(function(){
+            var href = $(this).attr('href');
+
+            $.get(href, function(data){
+                loadLinks($(data), /page\/([0-9]+)$/.exec(href)[1] || 1);
+            });
+        });
+    }
+
+    function go(){
+        $container = $('<div class="bigcplayer"><a href class=toggle>Open Player</a><ul /><div class=player /></div>');
+        $ul = $container.find('ul').css({padding:0,height:80,overflowY:'auto'});
+
+        loadLinks($('body'));
 
         $container
             .css({
                 position: 'fixed',
                 top: 0, left: 0,
                 width: 300,
+                padding: 3,
                 backgroundColor: '#000',
                 border: 'solid 2px #666',
-                borderRadius: '2px 2px 0 0'
+                borderWidth: '0 2px 2px 0',
+                borderRadius: '0 0 2px 0'
             })
             .appendTo('body')
             .delegate('a.toggle', 'click', function(){
@@ -69,10 +112,6 @@
                 $container.find('ul').slideUp();
 
                 return false;
-            })
-            .find('a').css({
-                color: '#FFF',
-                fontSize: 11
             });
 
         $container.find('ul').slideUp();
